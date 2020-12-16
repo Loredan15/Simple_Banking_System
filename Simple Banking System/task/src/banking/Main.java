714ffa44847db7e29d8e916123091a8ffb8fbb6b
m.shelepov <graf_ice@mail.ru>
2020-12-16 15:31:00 +0300

    Stage completed

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,43 +1,151 @@
 package banking;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        createAccount();
+  static Scanner scanner = new Scanner(System.in);
+  static Map<String, String> database = new HashMap<>();
 
+  public static void main(String[] args) {
+    mainMenu();
+//        for (Map.Entry<String, String> entry : database.entrySet()){
+//            System.out.println(entry);
+//        }
+
+  }
+
+  public static void mainMenu() {
+    printMenuWithoutLogin();
+    while (true) {
+
+      int itemMenu = scanner.nextInt();
+      switch (itemMenu) {
+        case 1:
+          createAccount();
+          printMenuWithoutLogin();
+          break;
+        case 2:
+          login();
+          break;
+        case 3:
+          System.out.println("\nBye!");
+          System.exit(1);
+        default:
+          System.out.println("Error: incorrect item menu");
+      }
     }
 
-    public static void printMenuWithoutLoging(){
-        System.out.println("1. Create an account");
-        System.out.println("2. Log into account");
-        System.out.println("3. Exit");
+  }
+
+  public static void printMenuWithoutLogin() {
+    System.out.println("1. Create an account");
+    System.out.println("2. Log into account");
+    System.out.println("3. Exit");
+  }
+
+  public static void printMenuWithLogin() {
+    System.out.println("1. Balance");
+    System.out.println("2. Log out");
+    System.out.println("0. Exit");
+  }
+
+  public static boolean createAccount() {
+    boolean result = false;
+    String cardNumber = "";
+    String pinCodeNumber = "";
+
+//    System.out.println("Enter your card number:");
+//    cardNumber = scanner.next();
+//
+//    System.out.println("Enter your PIN:");
+//    pinCodeNumber = scanner.next();
+//
+//    if (checkPinCode(pinCodeNumber) && checkCardNumber(cardNumber)) {
+//      result = true;
+//      System.out.println("Your card has been created");
+//      System.out.println("Your card number:");
+//      System.out.println(cardNumber);
+//      System.out.println("Your card PIN:");
+//      System.out.println(pinCodeNumber + "\n");
+//      database.put(cardNumber, pinCodeNumber);
+//    } else {
+//      System.out.println("Error: incorrect input");
+//    }
+
+    cardNumber = "4000001" + ThreadLocalRandom.current().nextLong(1_000_000_00,9_999_999_99);
+    pinCodeNumber = "" + ThreadLocalRandom.current().nextInt(1001, 10000);
+
+    System.out.println("Your card has been created");
+    System.out.println("Your card number:");
+    System.out.println(cardNumber);
+    System.out.println("Your card PIN:");
+    System.out.println(pinCodeNumber + "\n");
+    database.put(cardNumber, pinCodeNumber);
+
+    return result;
+  }
+
+  private static boolean checkPinCode(String pinCode) {
+    boolean result = true;
+    String regex = "^\\d{4}$";
+    if (!pinCode.matches(regex)) {
+      result = false;
     }
 
-    public static boolean createAccount(){
-        boolean result = false;
-        String pinCodeNumber = "";
+    return result;
+  }
 
-        System.out.println("Enter your card number:");
-        String cardNumber = scanner.nextLine();
+  private static boolean checkCardNumber(String cardNumber) {
+    boolean result = false;
 
-        System.out.println("Enter your PIN:");
-        if (scanner.hasNext()) {
-            pinCodeNumber = scanner.nextLine();
+    String iin = cardNumber.substring(0, 6);
+    if (iin.equals("400000")) {
+      result = true;
+    }
+
+    return result;
+  }
+
+  private static void login() {
+
+    System.out.println("Enter your card number:");
+    String cardNumber = scanner.next();
+
+    System.out.println("Enter your PIN:");
+    String pinCode = scanner.next();
+
+    if (!database.containsKey(cardNumber) || (!database.containsValue(pinCode))) {
+      System.out.println("Wrong card number or PIN!\n");
+    } else {
+      System.out.println("You have successfully logged in!\n");
+      printMenuWithLogin();
+      String itemMenuLogin = "";
+
+      while (true) {
+        itemMenuLogin = scanner.next();
+        switch (itemMenuLogin) {
+          case "1":
+            System.out.println("Balance: 0\n");
+            printMenuWithLogin();
+            break;
+          case "2":
+            System.out.println();
+            mainMenu();
+            break;
+          case "0":
+            System.exit(1);
+          default:
+            System.out.println("Error: incorrect item menu");
         }
-        if (checkPinCode(pinCodeNumber)) result = true;
-
-        return result;
+      }
     }
+  }
 
-    private static boolean checkPinCode(String pinCode){
-        boolean result = true;
-        String regex = "^\\d{4}$";
-        if (!pinCode.matches(regex)) result = false;
 
-        return result;
-    }
 }
